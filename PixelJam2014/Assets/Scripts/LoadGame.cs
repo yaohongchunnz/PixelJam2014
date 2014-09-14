835fca850c1ac91ddd6bafc70f18ff4c116e4a41
ZacharyF1991 <zachary.freiberg@gmail.com>
2014-09-14 17:49:41 +1200

    Menu Stuff

---
+++ PixelJam2014/Assets/Scripts/LoadGame.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadGame : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(LoadLevel());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	IEnumerator LoadLevel(){
+		yield return new WaitForSeconds(3.0f);
+		Application.LoadLevel("level1");
+	
+	}
+}
